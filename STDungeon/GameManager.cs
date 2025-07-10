b4c65b302cfa887428e29cf03317170441a913de
ChanMin0334 <52943035+ChanMin0334@users.noreply.github.com>
2025-07-10 14:36:55 +0900

    Create Prj, GameManage, Main

---
+++ STDungeon/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STDungeon
+{
+    internal class GameManager
+    {
+        // 게임 초기화
+        public void InitializeGame()
+        {
+            // 게임 데이터 초기화 코드
+        }
+
+        // 게임 시작
+        public void StartGame()
+        {
+            // 게임 시작 로직
+        }
+
+        // 플레이어 상태 출력
+        public void ShowPlayerStatus()
+        {
+            // 플레이어 상태 출력 코드
+        }
+
+        // 인벤토리 출력
+        public void ShowInventory()
+        {
+            // 인벤토리 출력 코드
+        }
+
+        // 전투 시작
+        public void StartBattle()
+        {
+            // 전투 로직
+        }
+
+        // 아이템 사용
+        public void UseItem()
+        {
+            // 아이템 사용 로직
+        }
+
+        // 게임 종료
+        public void EndGame()
+        {
+            // 게임 종료 처리
+        }
+
+        // 저장 기능
+        public void SaveGame()
+        {
+            // 게임 저장 로직
+        }
+
+        // 불러오기 기능
+        public void LoadGame()
+        {
+            // 게임 불러오기 로직
+        }
+    }
+}
